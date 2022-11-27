490ab1c9ea186d74505a96cadf0b14c2806582b1
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2022-11-27 20:46:59 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -24,5 +24,4 @@ const enviar = document.getElementById("contacto")
 
 enviar.addEventListener("click", () => {
     window.open("https://api.whatsapp.com/send?phone=5493462662288&text=");
-    console.log("asd");
 })
