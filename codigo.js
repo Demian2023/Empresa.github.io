7c28efed67c7bb0ba586c9f549f259fc35628770
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2022-11-27 20:42:58 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,6 +7,9 @@ const visible = (entradas) => {
     const entrada = entradas[0];
     if (entrada.isIntersecting) {
         efecto.classList.add("active");
+        setTimeout(() => {
+            efecto.classList.remove("active");
+        }, 2000)
     } else {
         efecto.classList.remove("active");
 
@@ -16,3 +19,10 @@ const visible = (entradas) => {
 const observer = new IntersectionObserver(visible);
 
 observer.observe(cuerpoPagina);
+
+const enviar = document.getElementById("contacto")
+
+enviar.addEventListener("click", () => {
+    window.open("https://api.whatsapp.com/send?phone=5493462662288&text=");
+    console.log("asd");
+})
