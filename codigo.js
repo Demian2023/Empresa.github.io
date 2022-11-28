fe21e754cdcff1213bb0ca104ddc80dd8c575cff
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2022-11-28 20:20:39 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,9 +1,4 @@
 "use strict";
-
-const observer = new IntersectionObserver(visible);
-
-observer.observe(cuerpoPagina);
-
 const enviar = document.getElementById("contacto")
 
 enviar.addEventListener("click", () => {
