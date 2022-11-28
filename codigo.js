d940f5c0950900a00b0f6c29e97cfba2f41e7b88
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2022-11-28 00:53:13 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,21 +1,5 @@
 "use strict";
 
-const cuerpoPagina = document.querySelector(".cuerpoPagina");
-const efecto = document.querySelector(".FX");
-
-const visible = (entradas) => {
-    const entrada = entradas[0];
-    if (entrada.isIntersecting) {
-        efecto.classList.add("active");
-        setTimeout(() => {
-            efecto.classList.remove("active");
-        }, 2000)
-    } else {
-        efecto.classList.remove("active");
-
-    }
-}
-
 const observer = new IntersectionObserver(visible);
 
 observer.observe(cuerpoPagina);
